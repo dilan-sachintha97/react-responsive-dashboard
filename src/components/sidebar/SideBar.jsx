fb64f8bfa47e261d28f9b8e15e2ab360570e648b
dilan-schintha97 <dil97an@gmail.com>
2023-10-14 14:01:52 +0530

    set nav bar

--- src/components/sidebar/SideBar.jsx
+++ src/components/sidebar/SideBar.jsx
@@ -2,8 +2,13 @@ import './SideBar.css'
 import Logo from '../../imgs/logo.png'
 
 import { sideBarData } from '../../data/Data'
+import {UilSignOutAlt} from "@iconscout/react-unicons"
+import { useState } from 'react'
 
 const SideBar = () => {
+
+const [selected ,setSelected] = useState(0)
+
   return (
     <div className="sideBar">
         
@@ -17,12 +22,14 @@ const SideBar = () => {
         <div className="menu">
             
             {sideBarData?.map((data,index)=>
-                <div key={index} className="menuItem">
+                <div onClick={()=>{setSelected((pre)=>index)}} key={index} className={ (selected===index)?"menuItem active":"menuItem"}>
                     <div><data.icon/></div>
                     <span>{data.heading}</span>
                 </div>
             )}
-            
+            <div className="menuItem">
+                <div><UilSignOutAlt/></div>
+            </div>
         </div>
     </div>
   )
